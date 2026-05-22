f7c7dd93c436798dfc23ccf1d8e683f24f0fd552
Toridesu <160250540+Toridesu@users.noreply.github.com>
2026-05-22 11:08:00 +0900

    Allow ECR cleanup during destroy

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -5,6 +5,7 @@ locals {
 resource "aws_ecr_repository" "app" {
   name                 = "${local.name_prefix}-api"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
